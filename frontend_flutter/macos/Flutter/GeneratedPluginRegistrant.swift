055da941dd724954611053119b4d337ee3f539cd
CSABD-inza <56353596+bambainza@users.noreply.github.com>
2026-07-17 23:24:26 +0000

    feat: implement offline synchronization service and add mission repository caching logic

--- frontend_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import file_selector_macos
 import flutter_secure_storage_macos
 import geolocator_apple
@@ -16,6 +17,7 @@ import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
