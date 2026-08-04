1191892002ac3b3e82bb1f4d45caa3a7b9ea5a8a
CSABD-inza <56353596+bambainza@users.noreply.github.com>
2026-08-04 13:07:48 +0000

    chore: configure custom build directory and Kotlin language versions for Android subprojects

--- frontend_flutter/android/build.gradle.kts
+++ frontend_flutter/android/build.gradle.kts
@@ -12,7 +12,7 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+val newBuildDir: Directory = layout.projectDirectory.dir("../../../build_temp")
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
