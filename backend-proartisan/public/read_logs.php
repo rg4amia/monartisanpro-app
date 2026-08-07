e23e0a6660d1d0ce65fdf4cd8daa6b5c2eaa4438
CSABD-inza <56353596+bambainza@users.noreply.github.com>
2026-08-07 17:54:17 +0000

    chore: remove temporary debug scripts

--- backend-proartisan/public/read_logs.php
+++
@@ -1,9 +0,0 @@
-<?php
-$logFile = __DIR__ . '/../storage/logs/laravel.log';
-if (file_exists($logFile)) {
-    $content = file($logFile);
-    $lastLines = array_slice($content, -100);
-    echo "<pre>" . htmlspecialchars(implode("", $lastLines)) . "</pre>";
-} else {
-    echo "Log file does not exist at: " . htmlspecialchars($logFile);
-}
