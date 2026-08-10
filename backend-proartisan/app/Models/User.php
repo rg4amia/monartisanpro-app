f863b12ef3c5b72f58888a4ec8df6f2ff203eea0
CSABD-inza <56353596+bambainza@users.noreply.github.com>
2026-08-10 15:41:42 +0000

    feat: implement SoftDeletes for the User model and add supporting database migration

--- backend-proartisan/app/Models/User.php
+++ backend-proartisan/app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasPermissions;
+    use HasApiTokens, HasFactory, Notifiable, HasPermissions, SoftDeletes;
 
     protected $fillable = [
         'email',
