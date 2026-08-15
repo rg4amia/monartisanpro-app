5d1a00be075cfcf0e9824aec9aa392a339cea92b
CSABD-inza <56353596+bambainza@users.noreply.github.com>
2026-08-15 00:11:46 +0000

    feat: create base application layout with Inertia and Vite configuration

--- backend-proartisan/resources/views/app.blade.php
+++ backend-proartisan/resources/views/app.blade.php
@@ -8,9 +8,10 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/img/prosartisan-logo.png?v=3">
+    <link rel="icon" href="/favicon.ico?v=3" sizes="any">
+    <link rel="icon" href="/favicon.svg?v=3" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=3">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
