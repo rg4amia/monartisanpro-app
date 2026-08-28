c59ef295d6674959e3170f1d251e6d6d53f73e6b
CSABD-inza <56353596+bambainza@users.noreply.github.com>
2026-08-28 21:09:36 +0000

    fix(auth): auto-normalize Ivorian phone formats (+225) in backend requests and frontend API

--- backend-proartisan/app/Http/Requests/Auth/SendOtpRequest.php
+++ backend-proartisan/app/Http/Requests/Auth/SendOtpRequest.php
@@ -11,6 +11,21 @@ class SendOtpRequest extends FormRequest
         return true;
     }
 
+    protected function prepareForValidation(): void
+    {
+        if ($this->has('phone')) {
+            $phone = preg_replace('/\s+/', '', (string) $this->phone);
+            if (str_starts_with($phone, '00225')) {
+                $phone = '+' . substr($phone, 2);
+            } elseif (str_starts_with($phone, '225')) {
+                $phone = '+' . $phone;
+            } elseif (preg_match('/^[0-9]{10}$/', $phone)) {
+                $phone = '+225' . $phone;
+            }
+            $this->merge(['phone' => $phone]);
+        }
+    }
+
     public function rules(): array
     {
         return [
