bb3e5e98719d285fa045f8925a94643b68503843
CSABD-inza <56353596+bambainza@users.noreply.github.com>
2026-09-01 13:03:52 +0000

    feat(analytics): integrate Google Analytics gtag.js G-JZ32VTRQSP on website and web apps

--- backend-proartisan/resources/views/app.blade.php
+++ backend-proartisan/resources/views/app.blade.php
@@ -6,6 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-JZ32VTRQSP"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-JZ32VTRQSP');
+    </script>
+
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     <link rel="icon" type="image/png" href="/favicon.png?v=4">
