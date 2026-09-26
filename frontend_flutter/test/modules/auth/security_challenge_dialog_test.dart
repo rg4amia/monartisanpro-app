7c761677e7b0302426c2c95bdc7638ad9dac2913
CSABD-inza <56353596+bambainza@users.noreply.github.com>
2026-09-26 15:02:13 +0000

    fix(flutter-test): suppression import inutilise et tri des directives pour flutter analyze

--- frontend_flutter/test/modules/auth/security_challenge_dialog_test.dart
+++ frontend_flutter/test/modules/auth/security_challenge_dialog_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get/get.dart';
 import 'package:frontend_flutter/core/theme/app_colors.dart';
 import 'package:frontend_flutter/modules/auth/widgets/login/login_tokens.dart';
 
